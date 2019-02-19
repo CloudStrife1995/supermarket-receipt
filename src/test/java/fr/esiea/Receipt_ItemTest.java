fd0704a2436b34a532b1405dc4b3bd8e9eb00f20
Raguideau <akiyaraguideau@gmail.com>
2019-02-19 21:33:50 +0100

    Changement des noms des variables dans Receipt_ItemTest pour faciliter la compréhension

--- src/test/java/fr/esiea/Receipt_ItemTest.java
+++ src/test/java/fr/esiea/Receipt_ItemTest.java
@@ -30,12 +30,12 @@ public class Receipt_ItemTest
         ReceiptItem receiptItem_sugar = new ReceiptItem(sugar,2.5, 2, 5);   
         ReceiptItem receiptItem_teaBox1 = new ReceiptItem(teaBox, 3, 2, 4);     
         ReceiptItem receiptItem_teaBox2 = new ReceiptItem(teaBox, 3, 2, 4);
-        ReceiptItem receiptItem_coffeeBox1 = new ReceiptItem(coffeeBox, 2, 4, 8);
-        ReceiptItem receiptItem_coffeeBox2 = new ReceiptItem(coffeeBox, 2, 5, 8);
-        ReceiptItem receiptItem_coffeeBox3 = new ReceiptItem(coffeeBox, 2, 4, 12);
-        ReceiptItem receiptItem_coffeeBox4 = new ReceiptItem(coffeeBox, 3, 4, 8);
-        ReceiptItem receiptItem_coffeeBox5 = new ReceiptItem(coffeeBox, 3, 2, 4);
-        ReceiptItem receiptItem_teaBox3 = receiptItem_teaBox1;   
+        ReceiptItem receiptItem_coffeeBox = new ReceiptItem(coffeeBox, 2, 4, 8);
+        ReceiptItem receiptItem_coffeeBox_different_price = new ReceiptItem(coffeeBox, 2, 5, 8);
+        ReceiptItem receiptItem_coffeeBox_different_total_price = new ReceiptItem(coffeeBox, 2, 4, 12);
+        ReceiptItem receiptItem_coffeeBox_different_quantity = new ReceiptItem(coffeeBox, 3, 4, 8);
+        ReceiptItem receiptItem_coffeeBox_same_value_as_teaBox = new ReceiptItem(coffeeBox, 3, 2, 4);
+        ReceiptItem receiptItem_teaBox1_bis = receiptItem_teaBox1;   
         //then the test on methods begin
         
 
@@ -43,13 +43,14 @@ public class Receipt_ItemTest
         assertThat(mugs.equals(receiptItem_mugs)).isNotEqualTo(true); 
 
         // Check the methods equals
-        assertThat(receiptItem_teaBox3.equals(receiptItem_teaBox1)).isEqualTo(true);
-        assertThat(receiptItem_coffeeBox1.equals(null)).isEqualTo(false);
-        assertThat(receiptItem_coffeeBox1.equals(receiptItem_coffeeBox2)).isEqualTo(false);
-        assertThat(receiptItem_coffeeBox1.equals(receiptItem_coffeeBox3)).isEqualTo(false);
-        assertThat(receiptItem_coffeeBox1.equals(receiptItem_coffeeBox4)).isEqualTo(false);
-        assertThat(receiptItem_teaBox1.equals(receiptItem_coffeeBox5)).isEqualTo(false);        
-
+        assertThat(receiptItem_teaBox1_bis.equals(receiptItem_teaBox1)).isEqualTo(true);
+        assertThat(receiptItem_coffeeBox.equals(coffeeBox)).isEqualTo(false);
+        assertThat(receiptItem_coffeeBox.equals(null)).isEqualTo(false);
+        assertThat(receiptItem_coffeeBox.equals(receiptItem_coffeeBox_different_price)).isEqualTo(false);
+        assertThat(receiptItem_coffeeBox.equals(receiptItem_coffeeBox_different_total_price)).isEqualTo(false);
+        assertThat(receiptItem_coffeeBox.equals(receiptItem_coffeeBox_different_quantity)).isEqualTo(false);
+        assertThat(receiptItem_teaBox1.equals(receiptItem_coffeeBox_same_value_as_teaBox)).isEqualTo(false);        
+        
         // check if the  receipt of the product is not null
         assertThat(receiptItem_mugs).isNotEqualTo(null);
 
@@ -68,7 +69,7 @@ public class Receipt_ItemTest
 
         // test on the hashCode
         assertThat(receiptItem_teaBox1.hashCode()).isEqualTo(receiptItem_teaBox2.hashCode());
-        assertThat(receiptItem_coffeeBox1.hashCode()).isNotEqualTo(receiptItem_coffeeBox2.hashCode());  
+        assertThat(receiptItem_coffeeBox.hashCode()).isNotEqualTo(receiptItem_coffeeBox_different_price.hashCode());  
     }
     
 }
