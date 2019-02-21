7f6124d167e855e90bb0e896b03529b4e456175e
BrunQuentin <32831076+BrunQuentin@users.noreply.github.com>
2019-02-21 23:36:57 +0100

    Modification du test dans ShoppingCartTest 	méthode testé:  addItemQuantity

--- src/test/java/fr/esiea/ShoppingCartTest.java
+++ src/test/java/fr/esiea/ShoppingCartTest.java
@@ -8,18 +8,26 @@ public class ShoppingCartTest
 {
 
 @Test  
-    public void ShoppingCart_Test() 
+
+    public void addItemQuantity_Test()
     {
-         Product candy_bag= new Product("Umbrella", ProductUnit.Each);
+        Product candy_bag= new Product("candy bag", ProductUnit.Each);
          //we create our cart to add item further;
          ShoppingCart cart = new ShoppingCart();
          //we want buy 5 candy bag for children
         cart.addItemQuantity(candy_bag,5);
 
-        // we create a list 
-        Map<Product, Double> items_into_cart = cart.productQuantities();
+        Map<Product, Double> items_quantities = cart.productQuantities();
+        double final_product_quantity = items_quantities.get(candy_bag);
 
-         assertThat(items_into_cart.containsKey(candy_bag)).isEqualTo(true);
+        //we caompare to the quantity in the cart 
+        double supposed_product_quantity = 5 ;
+
+        //test of the if else condition of the method addItemQuantity
+        assertThat(items_quantities.containsKey(candy_bag)).isEqualTo(true);
+         //test of the if else condition of the method addItemQuantity
+        assertThat(supposed_product_quantity).isEqualTo(5);
+      
 
     }
 } 
