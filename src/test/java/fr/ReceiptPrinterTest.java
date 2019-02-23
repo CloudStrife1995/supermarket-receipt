c6b28900cd9a641f11f0b75775a5e94bec3260ef
Raguideau <akiyaraguideau@gmail.com>
2019-02-23 22:13:33 +0100

    Modification du test sur le receipt pour ne pas prendre en compte l'ordre dans lequel les objets sont affiché

--- src/test/java/fr/ReceiptPrinterTest.java
+++ src/test/java/fr/ReceiptPrinterTest.java
@@ -27,51 +27,71 @@ public class ReceiptPrinterTest
         catalog.addProduct(artefact, 10);
         teller.addSpecialOffer(SpecialOfferType.TwoForAmount, artefact,5.00*2);
 
-        // We create a prodcut "necklace " without  special offer. 
+        // We create a prodcut "necklace" without  special offer. 
         Product necklace = new Product("necklace", ProductUnit.Each);
         catalog.addProduct(necklace, 20);
 
-       // In our current cart we have :
+       // We put the created product in a cart
         ShoppingCart cart = new ShoppingCart();  
         cart.addItemQuantity(artefact, 2);      
         cart.addItemQuantity(BioWatch, 3);
         cart.addItemQuantity(necklace, 1); 
         
-        // SupposedCartPrice is equal to 55€
-        double SupposedCartPrice = 1*20+ +2*10 +3*5; 
-
-        // RealCartPrice is equal to 40€ 
-        double RealCartPrice = teller.checksOutArticlesFrom(cart).getTotalPrice();
-        
-         // we check the cart 
+         // we get the printed receipt of the cart
       Receipt receipt_announced = teller.checksOutArticlesFrom(cart);
-      ReceiptPrinter receipt_printed = new ReceiptPrinter();   
+      ReceiptPrinter receipt_printed = new ReceiptPrinter(); 
+      String printed_ticket = receipt_printed.printReceipt(receipt_announced);
 
-        //Test the ReceiptPrinter ticket is not blank.
-      assertThat(receipt_printed.printReceipt(receipt_announced)).isNotBlank();
+        //Test the printed receipt is not blank.
+      assertThat(printed_ticket).isNotBlank();
 
 
-        // there we are the printed receipt give to the client.
-        String printed_ticket = 
+        // Thoses text must be in the printed receipt
+        String artefactPrice = 
          "artefact                           20.00\n"
         +
-        "  10.00 * 2\n"
-        +
+        "  10.00 * 2\n";
+
+        String bioWatchPrice = 
         "BioWatch                           15.00\n"
         +
-        "  5.00 * 3\n"
-        +
-        "necklace                           20.00\n"
-        +
-        "3 for 2(BioWatch)                  -5.00\n"
-        +
-        "2 for 10.0(artefact)              -10.00\n"
-        +
-        "\n"
-        +
-        "Total:                             40.00";
+        "  5.00 * 3\n";
 
-        assertThat(printed_ticket).isEqualTo(receipt_printed.printReceipt(receipt_announced));
+        String necklacePrice = "necklace                           20.00\n";
+        String bioWatchOffertDescrtiption = "3 for 2(BioWatch)                  -5.00\n";
+        String artefactOffertDescrtiption = "2 for 10.0(artefact)              -10.00\n";
+        String totalPrice ="Total:                             40.00";
+
+
+        // We test if the printed ticket contains the text above
+        assertThat(printed_ticket).containsOnlyOnce(artefactPrice);
+        assertThat(printed_ticket).containsOnlyOnce(bioWatchPrice);
+        assertThat(printed_ticket).containsOnlyOnce(necklacePrice);
+        assertThat(printed_ticket).containsOnlyOnce(bioWatchOffertDescrtiption);
+        assertThat(printed_ticket).containsOnlyOnce(artefactOffertDescrtiption);
+        assertThat(printed_ticket).containsOnlyOnce(totalPrice);
+
+        // We take one exemple of the printed suposed ticket to test the lenght of the receipt to make sure there isn't more than what we want
+        String printed_ticket_exemple = 
+                 "artefact                           20.00\n"
+                +
+                "  10.00 * 2\n"
+                +
+                "BioWatch                           15.00\n"
+                +
+                "  5.00 * 3\n"
+                +
+                "necklace                           20.00\n"
+                +
+                "3 for 2(BioWatch)                  -5.00\n"
+                +
+                "2 for 10.0(artefact)              -10.00\n"
+                +
+                "\n"
+                +
+                "Total:                             40.00";
+
+        assertThat(printed_ticket).hasSameSizeAs(printed_ticket_exemple);
 
     }
 }
