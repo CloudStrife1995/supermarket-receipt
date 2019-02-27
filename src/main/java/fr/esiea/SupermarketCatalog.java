71059d504c6603985c34978b54c4f0a9627e0910
CloudStrife1995 <stephanebarres@hotmail.com>
2019-02-27 15:46:32 +0100

    Modification de SupermarketCatalog pour le web service : - ajout de method deleteProduct - list of Products - list of prices

--- src/main/java/fr/esiea/SupermarketCatalog.java
+++ src/main/java/fr/esiea/SupermarketCatalog.java
@@ -1,8 +1,16 @@
 package fr.esiea;
 
 public interface SupermarketCatalog {
+	// add a product in the catalog
     void addProduct(Product product, double price);
-
+    // remove a product rom the catalog by his name
+    void deleteProduct(String name);
+    // list of products into the catalog
+    Map<String, Product> getListOfProducts();
+    // list of prices of products into the catalog
+    Map<String, Product> getListOfProductsPrices();
+    
+    // give the price by Unit of the product (in kilo or per each )
     double getUnitPrice(Product product);
 
 }
